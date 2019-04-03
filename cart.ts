75d1edd4af61bb532e6e0d4e38b7ad7a36453c82
Krzysztof Przybyla <kprzybyla@parasoft.com.pl>
2019-04-03 10:58:45 +0200

    Added 3 additional testables files

---
+++ cart.ts
@@ -0,0 +1,17 @@
+class cart { 
+    first_name: string; 
+    last_name: string; 
+  
+    constructor(fname: string, lname: string) 
+    { 
+        first_name = fname; 
+        last_name = lname; 
+    } 
+    getName(): string 
+    { 
+        var fullname: string = first_name + last_name; 
+        return fullname; 
+    } 
+} 
+  
+var author_name: Name; 
